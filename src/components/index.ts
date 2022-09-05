fc3e2b748b8de0f83eaeb93698f81ce0c29d7a4a
mkc <1292855448@qq.com>
2022-09-05 22:52:28 +0800

    feat: list组件

--- src/components/index.ts
+++ src/components/index.ts
@@ -3,8 +3,10 @@ import { App } from 'vue'
 import chooseIcon from './chooseIcon'
 import chooseArea from './chooseArea'
 import trend from './trend'
+import notification from './notification'
+import list from './list'
 
-const components = [chooseIcon, chooseArea, trend]
+const components = [chooseIcon, chooseArea, trend, notification, list]
 export default {
   install(app: App) {
     components.map((item) => app.use(item))
---
+++ src/views/notification/index.vue
@@ -0,0 +1,24 @@
+<template>
+  <div>
+    <m-notification :value="50"></m-notification>
+    <br />
+    <br />
+    <m-notification :value="50" :max="30"></m-notification>
+    <br />
+    <br />
+    <m-notification :value="50" :max="30" icon="Comment"></m-notification>
+    <br />
+    <br />
+    <m-notification :value="50" :max="30">
+      <template #default>
+        <m-list></m-list>
+      </template>
+    </m-notification>
+  </div>
+</template>
+
+<script setup lang='ts'>
+</script>
+
+<style lang='scss' scoped>
+</style>
